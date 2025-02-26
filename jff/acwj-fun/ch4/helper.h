2e84dd42080b890b8e58f091f79a73cf7af81602
Huan Zhou <me@per1cycle.org>
2025-02-26 13:46:40 +0800

    Start ch4

---
+++ jff/acwj-fun/ch4/helper.h
@@ -0,0 +1,46 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+struct token;
+
+FILE* input_file;
+int line_number = 1;
+char replace_char = 0;
+struct token g_token;
+int g_parse_digit = 0;
+
+
+enum TOKEN_TYPE {
+    EOF_T = 0,
+    ADD_T ,
+    MINUS_T,
+    MUL_T,
+    DIV_T,
+    INT_T,
+    UNKNOWN_T,
+};
+
+struct token 
+{
+    enum TOKEN_TYPE type;
+    int value;
+};
+
+const char* tokens_t[] = {
+    "eof",
+    "+",
+    "-",
+    "*",
+    "/",
+    "integer",
+    "Unknown type",
+};
+
+const char* tokens_a[] = {
+    "ast add",
+    "ast minus",
+    "ast mul",
+    "ast div",
+    "ast integer",
+};
+#endif // HELPER_H
