50587e6bccd1ce8edb8111129bb508b7d6cd4333
Huan Zhou <me@per1cycle.org>
2025-03-22 00:48:45 +0800

    Wierd, read binary input not as expected.

--- jff/playground/cnn.cc
+++ jff/playground/cnn.cc
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <cstring>
 #include <vector>
+#include <cassert>
 #include <iterator>
+
 
 /**
  * conv matrix
@@ -34,10 +36,61 @@ public:
     }
 
 public:
+    std::uint32_t Read4Byte()
+    {
+        assert(raw_data_.size() >= 4);
+        std::uint8_t block0 = raw_data_[0];
+        std::uint8_t block1 = raw_data_[1];
+        std::uint8_t block2 = raw_data_[2];
+        std::uint8_t block3 = raw_data_[3];
+        raw_data_.erase(raw_data_.begin(), raw_data_.begin() + 4);
+        return ((block0 << 24) | (block1 << 16) | (block2 << 8) | block3);
+    }
+
+    std::uint64_t Read8Byte()
+    {
+        assert(raw_data_.size() >= 8);
+        for(int i = 0; i < 64; i ++)
+        {
+            std::cout << std::hex << static_cast<std::uint32_t>(raw_data_[i]) << ' ';
+            if(i > 0 && i % 8 == 0)
+            {
+                std::cout << std::endl;
+            }
+        }
+        std::uint8_t block0 = raw_data_[0];
+        std::uint8_t block1 = raw_data_[1];
+        std::uint8_t block2 = raw_data_[2];
+        std::uint8_t block3 = raw_data_[3];
+        std::uint8_t block4 = raw_data_[4];
+        std::uint8_t block5 = raw_data_[5];
+        std::uint8_t block6 = raw_data_[6];
+        std::uint8_t block7 = raw_data_[7];
+        // std::cout << "Block 7 " << std::hex << static_cast<std::uint32_t>(block7) << std::endl;
+        raw_data_.erase(raw_data_.begin(), raw_data_.begin() + 8);
+
+        return 
+            (
+              (static_cast<std::uint64_t>(block0) << 56)
+            | (static_cast<std::uint64_t>(block1) << 48)
+            | (static_cast<std::uint64_t>(block2) << 40) 
+            | (static_cast<std::uint64_t>(block3) << 32) 
+            | (static_cast<std::uint64_t>(block4) << 24) 
+            | (static_cast<std::uint64_t>(block5) << 16) 
+            | (static_cast<std::uint64_t>(block6) << 8) 
+            | (static_cast<std::uint64_t>(block7) << 0)
+            );
+    }
+
     int Resolve()
     {
         // resolve a png data.
-        
+        std::uint64_t Header = Read8Byte();
+        if (Header != 0x89504e470d0a1a0a)
+        {
+            std::cerr << std::hex << Header << " Format error. \n";
+            return 1;
+        }
         return 0;
     }
 
@@ -65,6 +118,6 @@ int main(int argc, char **argv)
     }
 
     Mat image(argv[1]);
-    
+    image.Resolve();
     return 0;
 }
