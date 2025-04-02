15022bd5fb51471aa8d59c656d6cbc995c0acae1
Huan Zhou <me@per1cycle.org>
2025-04-03 00:36:54 +0800

    Update cl.

--- jff/playground/opencl/SGEMM/mat_mul_cl_v1.cpp
+++ jff/playground/opencl/SGEMM/mat_mul_cl_v1.cpp
@@ -25,12 +25,18 @@ int main(int argc, char** argv)
         usage();
         exit(1);
     }
-    
+
     // assuming Current directory is build/
     std::cout << "[===] Running v1 optimize for matmul." << std::endl;
     std::string kernel_path(argv[1]);
     std::string kernel_code = load_kernel_code(kernel_path);
     std::cout << "Kernel code: " << std::endl;
     std::cout << kernel_code << std::endl;
+
+    cl_platform_id platform_id;
+    cl_int tmp;
+    cl_uint platform_nums;
+    clGetPlatformIDs(tmp, &platform_id, &platform_nums);
+    std::cout << platform_nums << std::endl;
     return 0;
 }
