858499f636bdf47ff76f2d317299d01e12174028
Huan Zhou <me@per1cycle.org>
2025-04-04 16:18:51 +0800

    Add opencl skeleton.

---
+++ jff/playground/opencl/book/saxpy.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else 
+#include <CL/cl.h>
+#endif
+
+int main()
+{
+
+    return 0;
+}
