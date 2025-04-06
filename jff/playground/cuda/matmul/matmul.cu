cadb602586c6ab24a147fa215c6cb10afe7bda26
Huan Zhou <me@per1cycle.org>
2025-04-06 22:55:38 +0800

    Update matmul kernel.

--- jff/playground/cuda/matmul/matmul.cu
+++ jff/playground/cuda/matmul/matmul.cu
@@ -10,6 +10,19 @@ __global__ void mat_mul(float *a, float *b, float *c, int N, int M, int K)
     c[y * M + x] = tmp;
 }
 
+void load_numpy()
+{
+    std::cout << "import numpy as np" << std::endl;
+}
+
+void numpy_dot(std::string arr_a, std::string arr_b, std::string arr_compare)
+{
+    std::cout << "tmp = ";
+    std::cout << arr_a << ".dot(" << arr_b << ")" << std::endl;
+    std::cout << "res = np.allclose(" << "tmp" << ", " << arr_compare << ")" << std::endl;
+    std::cout << "print(res)" << std::endl;
+}
+
 void print_2d(float *a, int row, int col, std::string arr_name)
 {
     std::cout << arr_name << " = np.array(";
@@ -28,11 +41,21 @@ void print_2d(float *a, int row, int col, std::string arr_name)
     }
     std::cout << "])" << std::endl;
 }
+void gen_py(float *A, float *B, float *C, int N, int M, int K)
+{
+    load_numpy();
+    print_2d(A, N, K, "A");
+    print_2d(B, K, M, "B");
+    print_2d(C, N, M, "C");
+    numpy_dot("A", "B", "C");
+}
 
 int main()
 {
-    std::cout << "Cuda works" << std::endl;
-    const int N = 1024, M = 1024, K = 1024;
+    // const int N = 8192, M = 8192, K = 8192;
+    const int N = 2048, M = 2048, K = 2048;
+    // const int N = 1024, M = 1024, K = 1024;
+    // const int N = 4, M = 4, K = 4;
     float *A = new float[N * K];
     float *B = new float[K * M];
     float *C = new float[N * M];
@@ -66,13 +89,12 @@ int main()
     cudaMalloc((void **)&d_B, K * M * sizeof(float));
     cudaMalloc((void **)&d_C, N * M * sizeof(float));
 
-    cudaMemcpy(d_A, A, N * K * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, K * M * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_C, C, N * M * sizeof(float), cudaMemcpyHostToDevice);
-
     dim3 block(16, 16);
     dim3 grid((M + block.x - 1) / block.x, (N + block.y - 1) / block.y);
     auto start = std::chrono::high_resolution_clock::now();
+    cudaMemcpy(d_A, A, N * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, K * M * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_C, C, N * M * sizeof(float), cudaMemcpyHostToDevice);
     mat_mul<<<grid, block>>>(d_A, d_B, d_C, N, M, K);
     auto finish = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> elapsed = finish - start;
@@ -81,6 +103,10 @@ int main()
     cudaDeviceSynchronize();
     cudaMemcpy(C, d_C, N * M * sizeof(float), cudaMemcpyDeviceToHost);
 
-    print_2d(C, N, M, "C");
+    // gen_py(A, B, C, N, M, K);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
     return 0;
 }
