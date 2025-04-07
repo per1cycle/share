a1b6326c5060b8f95777095955fe25c90f59f07e
Huan Zhou <me@per1cycle.org>
2025-04-07 21:27:18 +0800

    Start impl share_ptr.

--- jff/playground/stl/smart_ptr.cc
+++ jff/playground/stl/smart_ptr.cc
@@ -5,3 +5,53 @@ class unique_ptr
 
 };
 
+template<typename T>
+class share_count;
+
+template<typename T>
+class share_ptr
+{
+
+public:
+    share_ptr(T foo): count_(1), foo_(foo) {}
+    share_ptr(): count_(0), foo_(nullptr) {}
+
+
+    ~share_ptr()
+    {
+       if(ptr_ && count_ == 1)
+       {
+            delete ptr_;
+       } 
+    }
+
+public:
+    void print_info()
+    {
+        std::cout << 
+    }
+private:
+    T *ptr_;
+    int count_;
+};
+
+template<typename T>
+class share_count
+{
+
+};
+
+template <typename T>
+share_ptr<T> make_share(T& foo)
+{
+
+}
+
+int main()
+{
+    int foo = 42;
+    share_ptr<int> a;
+
+
+    return 0;
+}
