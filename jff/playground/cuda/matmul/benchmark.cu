f0d29e2f263a88722ca6e5e0419cc64aeb863d4d
Huan Zhou <me@per1cycle.org>
2025-04-08 17:04:36 +0800

    Update benchmark.

--- jff/playground/cuda/matmul/benchmark.cu
+++ jff/playground/cuda/matmul/benchmark.cu
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <chrono> // for calculate duration.
 
 #include "common.cuh"
 
@@ -25,24 +27,41 @@ const std::vector<dim_t> dims =
     {.n = 4096, .m = 8192, .k = 8192},
 };
 
-const int REP = 50;
+const int REP = 10;
+
+const std::vector<std::string> opt_to_kernel = 
+{
+   "simple_matmul",
+    "cublas_sgemm"
+};
+
+void usage()
+{
+    std::cout << "Run kernel with: " << std::endl;
+    std::cout << "\t-0 simple_matmul" << std::endl;
+    std::cout << "\t-1 cublas_sgemm" << std::endl;
+}
 
 void benchmark_info(uint n, uint m, uint k)
 {
     std::cout << "Run benchmark with params: " << "N: " \
         << n << ", M: " << m << ", K: " << k << std::endl;
-    std::cout << "work load: " << 2.0f * n * m * k * 1e-9 << "GFLOP" << std::endl;
+    std::cout << "work load: " << 2.0f * n * m * k * 1e-9 << " GFLOP" << std::endl;
 }
 
-void benchmark_result(int N, int M, int K, float total_elapse_time)
+void benchmark_result(int N, int M, int K, float total_elapse_time_in_milisecond)
 {
-
+    float total_elapse_time_in_second = total_elapse_time_in_milisecond / REP / 1000.0f; // to seconds.
+    std::cout << "FLOPS: " << 2.0f * M * N * K / total_elapse_time_in_second / 1000 / 1000 / 1000 << " GFLOPS." << std::endl;
 }
 
-void benchmark()
+void benchmark(uint &opt)
 {
-    for(auto dim: dims)
+    std::cout << "Running with option: " << opt << std::endl;
+
+    for(int i = 0; i < dims.size() - 3; i ++)
     {
+        dim_t dim = dims[i];
         uint N = dim.n;
         uint M = dim.m;
         uint K = dim.k;
@@ -53,18 +72,29 @@ void benchmark()
 
         // benchmark_info(N, M, K);
         gen_data(h_a, h_b, h_c, N, M, K);
-        float total_elapse_time = 0.0f;
+        float total_elapse_time_in_milisecond = 0.0f;
 
         for(int r = 0; r < REP; r ++)
         {
+            auto start = std::chrono::high_resolution_clock::now();
             simple_matmul(h_a, h_b, h_c, N, M, K);
+            auto finish = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> elapsed = finish - start;
+            total_elapse_time_in_milisecond += elapsed.count();
         }
 
-        benchmark_result(N, M, K, total_elapse_time);
+        benchmark_result(N, M, K, total_elapse_time_in_milisecond);
     }
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    benchmark();
+    if(argc < 2)
+    {
+        usage();
+        exit(1);
+    }
+    uint opt = atoi(argv[1]);
+
+    benchmark(opt);
 }
