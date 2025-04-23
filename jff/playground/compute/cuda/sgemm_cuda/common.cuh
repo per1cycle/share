cca51a619a27538e64a5ad93ea2c6c025948d1a3
Huan Zhou <me@per1cycle.org>
2025-04-24 00:17:10 +0800

    Impl naive sgemm.

--- jff/playground/compute/cuda/sgemm_cuda/common.cuh
+++ jff/playground/compute/cuda/sgemm_cuda/common.cuh
@@ -2,6 +2,7 @@
 #define COMMON_CUH
 
 #include <random>
+#define abs(x) ((x > 0)? x: - x)
 
 void generate_float_matrix(float *out, int row, int column)
 {
@@ -25,6 +26,30 @@ void print_array(float *arr, int row, int column)
         }
         std::cout << std::endl;
     }
+}
+
+void cmp_result(float *res, float *a, float *b, int N, int M, int K)
+{
+    float *tmp = new float[N * K];
+    memset(tmp, 0, sizeof(float) * N * K);
+    for(int i = 0; i < N; i ++)
+    {
+        for(int j = 0; j < K; j ++)
+        {
+            for(int k = 0; k < M; k ++)
+            {
+                tmp[i * K + j] += a[i * M + k] * b[j + k * K];
+            }
+
+            if(abs(res[i * K + j] - tmp[i * K + j]) > 1e-2)
+            {
+                std::cout << "Result error.\n";
+                exit(1);
+            }
+        }
+    }
+    std::cout << "Correct! be proud of it!\n";
+
 }
 
 #endif
