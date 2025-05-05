b78e28b1c05441c18a2bc5bef8b4fa9c56f01101
Huan Zhou <me@per1cycle.org>
2025-05-05 14:58:49 +0800

    Add win

---
+++ jff/playground/compute/cuda/gemm_win/Kernels.cuh
@@ -0,0 +1,106 @@
+#ifndef KERNEL_CUH
+#define KERNEL_CUH
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <cublas_v2.h>
+
+#include <iostream>
+
+using uint = unsigned int;
+
+void generate_float_matrix(float* out, int row, int column)
+{
+    srand(time(NULL));
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            out[i * column + j] = (float)rand() / (float)(INT32_MAX);
+        }
+    }
+}
+
+void print_array(float* arr, int row, int column)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            std::cout << arr[i * column + j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void cmp_result(float* res, float* a, float* b, int M, int N, int K)
+{
+    float* tmp = new float[M * N];
+    memset(tmp, 0, sizeof(float) * M * N);
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < K; k++)
+            {
+                tmp[i * N + j] += a[i * K + k] * b[j + k * N];
+            }
+
+            if (abs(res[i * K + j] - tmp[i * K + j]) > 1e-2)
+            {
+                std::cout << "Result error.\n";
+                exit(1);
+            }
+        }
+    }
+    std::cout << "Correct! be proud of it!\n";
+
+}
+
+__global__ void sgemm_naive(
+    uint M, uint N, uint K,
+	float* a, float* b, float* c)
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	if (x < M && y < N)
+	{
+		float sum = 0;
+		for (int k = 0; k < K; k++)
+		{
+			sum += a[x * K + k] * b[k * N + y];
+		}
+		c[x * N + y] = sum;
+	}
+}
+
+void run_kernel(int kernel_type,
+	uint M, uint N, uint K,
+	float *a, float *b, float *c)
+{
+    switch (kernel_type)
+    {
+    case 0: // cublas
+    {
+		cublasHandle_t handle;
+		cublasCreate(&handle);
+		float alpha = 1.0f;
+		float beta = 0.0f;
+		cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
+			M, N, K,
+			&alpha, a, M,
+			b, K,
+			&beta, c, M);
+        break;
+    }
+    case 1:
+    {
+		dim3 grid_dim = dim3(M / 32, N / 32, 1);
+		dim3 block_dim = dim3(32, 32, 1);
+		sgemm_naive << <grid_dim, block_dim >> > (M, N, K, a, b, c);
+        break;
+    }
+    default:
+        break;
+    }
+}
+#endif // !KERNEL_CUH
---
+++ jff/playground/compute/cuda/gemm_win/Main.cu
@@ -0,0 +1,68 @@
+#include "Kernels.cuh"
+#include <iostream>
+
+#include <cuda_runtime.h>
+
+int main(int argc, char ** argv)
+{
+	if (argc != 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " <mode> <N> " << std::endl;
+		return -1;
+	}
+
+	unsigned int N = atoi(argv[2]);
+	int mode = atoi(argv[1]);
+	std::cout << "Running with mode: " << mode << std::endl;
+	unsigned int size = N * N;
+
+	float* h_a, * h_b, * h_c;
+
+	h_a = (float*)malloc(size * sizeof(float));
+	h_b = (float*)malloc(size * sizeof(float));
+	h_c = (float*)malloc(size * sizeof(float));
+
+	generate_float_matrix(h_a, N, N);
+	generate_float_matrix(h_b, N, N);
+
+	float* d_a, * d_b, * d_c;
+	cudaMalloc((void**)&d_a, size * sizeof(float));
+	cudaMalloc((void**)&d_b, size * sizeof(float));
+	cudaMalloc((void**)&d_c, size * sizeof(float));
+
+	cudaMemcpy(d_a, h_a, size * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, h_b, size * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_c, h_c, size * sizeof(float), cudaMemcpyHostToDevice);
+
+	cudaEvent_t start, stop;
+	float elapsed = 0.0f;
+    
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
+
+	run_kernel(mode, N, N, N, d_a, d_b, d_c);
+
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsed, start, stop);
+
+	// show result
+	float flop = 1.0 * N * N * (2 * N + 1);
+	float gflop = flop / 1000000000.0f;
+	elapsed = elapsed / 1000.0f; // to second
+
+	std::cout
+		<< "Time:                                   \t" << elapsed << " s.\n"
+		<< "GFlop:                                  \t" << gflop << "\n"
+		<< "GFLOPS:                                 \t" << gflop / elapsed << "\n";
+
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+	free(h_a);
+	free(h_b);
+	free(h_c);
+
+	return 0;
+}
