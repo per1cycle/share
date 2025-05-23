d182b16f5e4c8bafc75fa46f97949a868d19a5b3
Huan Zhou <me@per1cycle.org>
2025-05-23 23:07:41 +0800

    Update cuda skeleton.cu

--- jff/playground/compute/cuda/playground/skeleton.cu
+++ jff/playground/compute/cuda/playground/skeleton.cu
@@ -1,28 +1,43 @@
 #include <iostream>
-// #include <common.cuh>
+#include <cuda_runtime.h>
+#include <cuda_fp16.h>
+#include "common.cuh"
+template<const uint BM, const uint BN, const uint BK>
+__global__ void kernel(uint M, uint N, uint K, half *a, half *b, half *c)
+{
+    {
+        printf("Thread: %d\n", threadIdx.x);
+    }
+}
 int main()
 {
     std::cout << "Cuda works" << std::endl;
-    int M = 1024, N = 1024, K = 1024;
+    int M = 1024, N = 16, K = 1024;
 
-    float *h_a = (float*) malloc(sizeof(float) * M * K);
-    float *h_b = (float*) malloc(sizeof(float) * K * N);
-    float *h_c = (float*) malloc(sizeof(float) * M * N);
+    half *h_a = (half*) malloc(sizeof(half) * M * K);
+    half *h_b = (half*) malloc(sizeof(half) * K * N);
+    half *h_c = (half*) malloc(sizeof(half) * M * N);
 
-    float *d_a, *d_b, *d_c;
+    utils::generate_T_matrix(h_a, M, K);
+    utils::generate_T_matrix(h_b, K, N);
 
-    cudaMalloc((void **)&d_a, M * K * sizeof(float));
-    cudaMalloc((void **)&d_b, K * N * sizeof(float));
-    cudaMalloc((void **)&d_c, M * N * sizeof(float));
+    half *d_a, *d_b, *d_c;
 
-    cudaMemcpy(d_a, h_a, M * K * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_a, K * N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_c, h_a, M * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&d_a, M * K * sizeof(half));
+    cudaMalloc((void **)&d_b, K * N * sizeof(half));
+    cudaMalloc((void **)&d_c, M * N * sizeof(half));
+
+    cudaMemcpy(d_a, h_a, M * K * sizeof(half), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_a, K * N * sizeof(half), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_c, h_a, M * N * sizeof(half), cudaMemcpyHostToDevice);
     ///////////////////////////////////////////////////////////////////////////////////////
     // run kernel here
-
+    const uint BM = 16, BN = 16, BK = 512;
+    dim3 grid_dim = {M / BM, N / BN, 1};
+    dim3 blk_dim = {BM * BN};
+    kernel<BM, BN, BK><<<grid_dim, blk_dim>>>(M, N, K, d_a, d_b, d_c);
     ///////////////////////////////////////////////////////////////////////////////////////
-    cudaMemcpy(h_c, d_c, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_c, d_c, M * N * sizeof(half), cudaMemcpyDeviceToHost);
 
     cudaFree(d_a);
     cudaFree(d_b);
